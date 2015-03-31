b849c9dc57757c58279ce37306d55cf7fa27537f
Jon Lefcheck <jslefche@vims.edu>
2015-03-31 11:23:57 -0400

    Minor fixes to MSE

--- R/multSE.R
+++ R/multSE.R
@@ -4,7 +4,49 @@ multSE = function(D, nresamp = 10000, group = 1, permanova = F, ... ) {
   # Ensure distance matrix is of class==matrix and not class==dist
   D = as.matrix(D)
   
-  if(permanova == F) {
+  if(permanova == T) {
+
+    # Conduct permutation (replace = F) and boostrapped (replace = T) resampling
+    mult.SE.list = lapply(c(F, T), function(replace) {
+      
+      # Bootstrap subsetted distance matrix by smallest sample size across all groups
+      do.call(cbind, lapply(2:min(table(group)), function(nsub) {
+        # And for each level of number of resamples
+        do.call(rbind, lapply(1:nresamp, function(iresamp) {
+          
+          cols = do.call(c, lapply(unique(group), function(igroup) 
+            # Randomly sample columns numbers for each group
+            sample(which(group == igroup), size = nsub, replace = replace) ) )
+          
+          # Sample distance matrix from column numbers
+          D.samp = D[cols, cols]
+          
+          # Calculate multivariate SE
+          group.resamp = factor(rep(1:length(unique(group)), each = nsub))
+          N = dim(D.samp)[1]
+          g = length(levels(factor(group.resamp)))
+          X = model.matrix( ~ factor(group.resamp))
+          H = X %*% solve(t(X) %*% X) %*% t(X)
+          I = diag(N)
+          A = -0.5 * D.samp^2 
+          G = A - apply(A, 1, mean) %o% rep(1, N) - rep(1, N) %o% apply(A, 2, mean) + mean(A) 
+          MSE = sum(diag((I - H) %*% G))/(N - g)   
+          sqrt(MSE/nsub)
+          
+        } ) )
+      } ) )
+    } )
+    
+    # Return data.frame with means and quantiles
+    df = data.frame(
+      n.samp = 1:length(colMeans(mult.SE.list[[1]])),
+      means =  colMeans(mult.SE.list[[1]]),
+      lower.ci = apply(mult.SE.list[[2]], 2, function(x) quantile(x, prob = 0.025, na.rm = T)) + 
+        (colMeans(mult.SE.list[[1]]) - colMeans(mult.SE.list[[2]])),
+      upper.ci = apply(mult.SE.list[[2]], 2, function(x) quantile(x, prob = 0.975, na.rm = T)) + 
+        (colMeans(mult.SE.list[[1]]) - colMeans(mult.SE.list[[2]])) )
+    
+  } else {
     
     # Conduct bootstrapping for each group
     df = do.call(rbind, lapply(unique(group), function(igroup) {
@@ -44,51 +86,8 @@ multSE = function(D, nresamp = 10000, group = 1, permanova = F, ... ) {
           (colMeans(mult.SE.list[[1]]) - colMeans(mult.SE.list[[2]])) )
       
     } ) )
-    
-  } else if(permanova == T) {
-    
-    # Conduct permutation (replace = F) and boostrapped (replace = T) resampling
-    mult.SE.list = lapply(c(F, T), function(replace) {
-      
-      # Bootstrap subsetted distance matrix by smallest sample size across all groups
-      do.call(cbind, lapply(2:min(table(group)), function(nsub) {
-        # And for each level of number of resamples
-        do.call(rbind, lapply(1:nresamp, function(iresamp) {
-          
-          cols = do.call(c, lapply(unique(group), function(igroup) 
-            # Randomly sample columns numbers for each group
-            sample(which(group == igroup), size = nsub, replace = replace) ) )
-          
-          # Sample distance matrix from column numbers
-          D.samp = D[cols, cols]
-            
-          # Calculate multivariate SE
-          group.resamp = factor(rep(1:length(unique(group)), each = nsub))
-          N = dim(D.samp)[1]
-          g = length(levels(factor(group.resamp)))
-          X = model.matrix( ~ factor(group.resamp))
-          H = X %*% solve(t(X) %*% X) %*% t(X)
-          I = diag(N)
-          A = -0.5 * D.samp^2 
-          G = A - apply(A, 1, mean) %o% rep(1, N) - rep(1, N) %o% apply(A, 2, mean) + mean(A) 
-          MSE = sum(diag((I - H) %*% G))/(N - g)   
-          sqrt(MSE) / nsub
-          
-        } ) )
-      } ) )
-    } )
-    
-    # Return data.frame with means and quantiles
-    df = data.frame(
-      n.samp = 1:length(colMeans(mult.SE.list[[1]])),
-      means =  colMeans(mult.SE.list[[1]]),
-      lower.ci = apply(mult.SE.list[[2]], 2, function(x) quantile(x, prob = 0.025, na.rm = T)) + 
-        (colMeans(mult.SE.list[[1]]) - colMeans(mult.SE.list[[2]])),
-      upper.ci = apply(mult.SE.list[[2]], 2, function(x) quantile(x, prob = 0.975, na.rm = T)) + 
-        (colMeans(mult.SE.list[[1]]) - colMeans(mult.SE.list[[2]])) )
-    
   }
     
-  return(df)
+  return(df) 
 
-}
+  }
