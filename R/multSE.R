3da23696ab01e900b7214bc6a65599eaa90ed3b5
Jon Lefcheck <jslefche@vims.edu>
2015-03-31 11:02:05 -0400

    Rename function and include PERMANOVA

---
+++ R/multSE.R
@@ -0,0 +1,94 @@
+# Function to conduct bootstrap and permutation tests
+multSE = function(D, nresamp = 10000, group = 1, permanova = F, ... ) {
+  
+  # Ensure distance matrix is of class==matrix and not class==dist
+  D = as.matrix(D)
+  
+  if(permanova == F) {
+    
+    # Conduct bootstrapping for each group
+    df = do.call(rbind, lapply(unique(group), function(igroup) {
+      
+      # Subset distance matrix by group
+      subset.D = D[group == igroup, group == igroup]
+      
+      # Conduct permutation (replace = F) and boostrapped (replace = T) resampling
+      mult.SE.list = lapply(c(F, T), function(replace) {
+        
+        # Bootstrap subsetted distance matrix by each sample size
+        do.call(cbind, lapply(2:ncol(subset.D), function(nsub) {
+          # And for each level of number of resamples
+          do.call(rbind, lapply(1:nresamp, function(iresamp) {
+            
+            # Randomly sample distance matrix
+            D.samp = subset.D[sample(1:ncol(subset.D), size = nsub, replace), sample(1:ncol(subset.D), size = nsub, replace)]
+            
+            # Calculate multivariate SE
+            n = dim(as.matrix(D.samp))[1]
+            ss = sum(D.samp^2)/n
+            v = ss/(n-1)
+            sqrt(v/n)
+  
+          } ) ) 
+        } ) )
+      } )
+      
+      # Return data.frame with means and quantiles
+      data.frame(
+        group = igroup,
+        n.samp = 1:length(colMeans(mult.SE.list[[1]])),
+        means =  colMeans(mult.SE.list[[1]]),
+        lower.ci = apply(mult.SE.list[[2]], 2, function(x) quantile(x, prob = 0.025, na.rm = T)) + 
+          (colMeans(mult.SE.list[[1]]) - colMeans(mult.SE.list[[2]])),
+        upper.ci = apply(mult.SE.list[[2]], 2, function(x) quantile(x, prob = 0.975, na.rm = T)) + 
+          (colMeans(mult.SE.list[[1]]) - colMeans(mult.SE.list[[2]])) )
+      
+    } ) )
+    
+  } else if(permanova == T) {
+    
+    # Conduct permutation (replace = F) and boostrapped (replace = T) resampling
+    mult.SE.list = lapply(c(F, T), function(replace) {
+      
+      # Bootstrap subsetted distance matrix by smallest sample size across all groups
+      do.call(cbind, lapply(2:min(table(group)), function(nsub) {
+        # And for each level of number of resamples
+        do.call(rbind, lapply(1:nresamp, function(iresamp) {
+          
+          cols = do.call(c, lapply(unique(group), function(igroup) 
+            # Randomly sample columns numbers for each group
+            sample(which(group == igroup), size = nsub, replace = replace) ) )
+          
+          # Sample distance matrix from column numbers
+          D.samp = D[cols, cols]
+            
+          # Calculate multivariate SE
+          group.resamp = factor(rep(1:length(unique(group)), each = nsub))
+          N = dim(D.samp)[1]
+          g = length(levels(factor(group.resamp)))
+          X = model.matrix( ~ factor(group.resamp))
+          H = X %*% solve(t(X) %*% X) %*% t(X)
+          I = diag(N)
+          A = -0.5 * D.samp^2 
+          G = A - apply(A, 1, mean) %o% rep(1, N) - rep(1, N) %o% apply(A, 2, mean) + mean(A) 
+          MSE = sum(diag((I - H) %*% G))/(N - g)   
+          sqrt(MSE) / nsub
+          
+        } ) )
+      } ) )
+    } )
+    
+    # Return data.frame with means and quantiles
+    df = data.frame(
+      n.samp = 1:length(colMeans(mult.SE.list[[1]])),
+      means =  colMeans(mult.SE.list[[1]]),
+      lower.ci = apply(mult.SE.list[[2]], 2, function(x) quantile(x, prob = 0.025, na.rm = T)) + 
+        (colMeans(mult.SE.list[[1]]) - colMeans(mult.SE.list[[2]])),
+      upper.ci = apply(mult.SE.list[[2]], 2, function(x) quantile(x, prob = 0.975, na.rm = T)) + 
+        (colMeans(mult.SE.list[[1]]) - colMeans(mult.SE.list[[2]])) )
+    
+  }
+    
+  return(df)
+
+}
