d1ffd6253031ab4f1b800beb58e2aa15cee1acc4
Jon Lefcheck <jslefche@vims.edu>
2015-07-20 17:12:11 -0400

    Minor formatting changes

--- R/multSE.R
+++ R/multSE.R
@@ -1,29 +1,37 @@
 # Function to conduct bootstrap and permutation tests
-multSE = function(D, nresamp = 10000, group = 1, permanova = F, ... ) {
+multSE = function(D, nresamp = 10000, group = 1, permanova = TRUE, ... ) {
   
   # Ensure distance matrix is of class==matrix and not class==dist
   D = as.matrix(D)
   
   if(permanova == T) {
-
+    
     # Conduct permutation (replace = F) and boostrapped (replace = T) resampling
-    mult.SE.list = lapply(c(F, T), function(replace) {
-
+    mult.SE.list = lapply(c(FALSE, TRUE), function(replace) {
+      
       # Remove groups with only a single replicate
       if(min(table(group)) == 1) {
+        
         remove = names(table(group)[table(group) == min(table(group))])
+        
         D = D[!group %in% remove, !group %in% remove]
+        
         group = group[!group %in% remove]
+        
         warning("Groups with 1 replicate have been removed from the analysis!")
+        
       }
-            
+      
       # Bootstrap subsetted distance matrix by smallest sample size across all groups
       do.call(cbind, lapply(2:min(table(group)), function(nsub) {
-
+        
         # Define model parameters
         group.resamp = factor(rep(1:length(unique(group)), each = nsub))
+        
         g = length(levels(factor(group.resamp)))
+        
         X = model.matrix( ~ factor(group.resamp))
+        
         H = X %*% solve(t(X) %*% X) %*% t(X)
         
         # And for each level of number of resamples
@@ -38,20 +46,30 @@ multSE = function(D, nresamp = 10000, group = 1, permanova = F, ... ) {
           
           # Calculate multivariate SE based on residuals from PERMANOVA
           N = dim(D.samp)[1]
+          
           I = diag(N)
+          
           A = -0.5 * D.samp^2 
+          
           G = A - apply(A, 1, mean) %o% rep(1, N) - rep(1, N) %o% apply(A, 2, mean) + mean(A) 
+          
           MSE = sum(diag((I - H) %*% G))/(N - g)   
+          
           sqrt(MSE/nsub)
           
         } ) )
+        
       } ) )
+      
     } )
     
     #Calculate means and quantiles
     means = colMeans(mult.SE.list[[1]], na.rm = T)
+    
     means.p = colMeans(mult.SE.list[[2]], na.rm = T)
+    
     lower.ci = apply(mult.SE.list[[2]], 2, function(x) quantile(x, prob = 0.025, na.rm = T))
+    
     upper.ci = apply(mult.SE.list[[2]], 2, function(x) quantile(x, prob = 0.975, na.rm = T))
     
     # Return data.frame with means and quantiles
@@ -77,7 +95,7 @@ multSE = function(D, nresamp = 10000, group = 1, permanova = F, ... ) {
           lower.ci = NA,
           upper.ci = NA) 
         
-        } else {
+      } else {
         
         # Conduct permutation (replace = F) and boostrapped (replace = T) resampling
         mult.SE.list = lapply(c(F, T), function(replace) {
@@ -92,18 +110,24 @@ multSE = function(D, nresamp = 10000, group = 1, permanova = F, ... ) {
               
               # Calculate multivariate SE based on SS
               n = dim(as.matrix(D.samp))[1]
+              
               ss = sum(D.samp^2)/n
+              
               v = ss/(n-1)
+              
               sqrt(v/n)
-    
+              
             } ) ) 
           } ) )
         } )
         
         #Calculate means and quantiles
         means = colMeans(mult.SE.list[[1]], na.rm = T)
+        
         means.p = colMeans(mult.SE.list[[2]], na.rm = T)
+        
         lower.ci = apply(mult.SE.list[[2]], 2, function(x) quantile(x, prob = 0.025, na.rm = T))
+        
         upper.ci = apply(mult.SE.list[[2]], 2, function(x) quantile(x, prob = 0.975, na.rm = T))
         
         # Return data.frame with means and quantiles
@@ -114,11 +138,11 @@ multSE = function(D, nresamp = 10000, group = 1, permanova = F, ... ) {
           lower.ci = lower.ci + (means - means.p),
           upper.ci = upper.ci + (means - means.p) )
         
-        }
-        
-      } ) )
+      }
+      
+    } ) )
   }
-    
+  
   return(df) 
-
-  }
+  
+}
