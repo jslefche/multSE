b60371cda6b381c888286666f9df780b3cbe44b9
Jon Lefcheck <jslefche@vims.edu>
2017-09-18 16:40:39 -0400

    Updated function with improved syntax

--- R/multSE.R
+++ R/multSE.R
@@ -1,24 +1,32 @@
-# Function to conduct bootstrap and permutation tests
-multSE = function(D, nresamp = 10000, group = 1, permanova = TRUE, ... ) {
+#' multSE: multivariate dissimilarity-based standard errors
+#' 
+#' @param D a sample-by-species distance matrix
+#' @param group an optional vector of groups
+#' @param nressamp number of resamples in bootstrapping
+#' @param permanova, whether standard errors should be computed across all groups (TRUE) or individually for each group (FALSE)
+#' 
+#' @return a data.frame with the group (if specified), the means +/- 95% confidence intervals for each level of N (samples)
+
+multSE <- function(D, group, nresamp = 10000, permanova = TRUE) {
   
   # Ensure distance matrix is of class==matrix and not class==dist
-  D = as.matrix(D)
+  D <- as.matrix(D)
   
-  if(permanova == T) {
+  if(permanova == TRUE & !missing(group)) {
     
     # Conduct permutation (replace = F) and boostrapped (replace = T) resampling
-    mult.SE.list = lapply(c(FALSE, TRUE), function(replace) {
+    mult.SE.list <- lapply(c(FALSE, TRUE), function(replace) {
       
       # Remove groups with only a single replicate
       if(min(table(group)) == 1) {
         
-        remove = names(table(group)[table(group) == min(table(group))])
+        remove <- names(table(group)[table(group) == min(table(group))])
         
-        D = D[!group %in% remove, !group %in% remove]
+        D <- D[!group %in% remove, !group %in% remove]
         
-        group = group[!group %in% remove]
+        group <- group[!group %in% remove]
         
-        warning("Groups with 1 replicate have been removed from the analysis!")
+        message("Groups with 1 replicate have been removed from the analysis!")
         
       }
       
@@ -26,34 +34,34 @@ multSE = function(D, nresamp = 10000, group = 1, permanova = TRUE, ... ) {
       do.call(cbind, lapply(2:min(table(group)), function(nsub) {
         
         # Define model parameters
-        group.resamp = factor(rep(1:length(unique(group)), each = nsub))
+        group.resamp <- factor(rep(1:length(unique(group)), each = nsub))
         
-        g = length(levels(factor(group.resamp)))
+        g <- length(levels(factor(group.resamp)))
         
-        X = model.matrix( ~ factor(group.resamp))
+        X <- model.matrix( ~ factor(group.resamp))
         
-        H = X %*% solve(t(X) %*% X) %*% t(X)
+        H <- X %*% solve(t(X) %*% X) %*% t(X)
         
         # And for each level of number of resamples
         do.call(rbind, lapply(1:nresamp, function(iresamp) {
           
-          cols = do.call(c, lapply(unique(group), function(igroup) 
+          cols <- do.call(c, lapply(unique(group), function(igroup) 
             # Randomly sample columns numbers for each group
             sample(which(group == igroup), size = nsub, replace = replace) ) )
           
           # Sample distance matrix from column numbers
-          D.samp = D[cols, cols]
+          D.samp <- D[cols, cols]
           
           # Calculate multivariate SE based on residuals from PERMANOVA
-          N = dim(D.samp)[1]
+          N <- dim(D.samp)[1]
           
-          I = diag(N)
+          I <- diag(N)
           
-          A = -0.5 * D.samp^2 
+          A <- -0.5 * D.samp^2 
           
-          G = A - apply(A, 1, mean) %o% rep(1, N) - rep(1, N) %o% apply(A, 2, mean) + mean(A) 
+          G <- A - apply(A, 1, mean) %o% rep(1, N) - rep(1, N) %o% apply(A, 2, mean) + mean(A) 
           
-          MSE = sum(diag((I - H) %*% G))/(N - g)   
+          MSE <- sum(diag((I - H) %*% G))/(N - g)   
           
           sqrt(MSE/nsub)
           
@@ -64,16 +72,16 @@ multSE = function(D, nresamp = 10000, group = 1, permanova = TRUE, ... ) {
     } )
     
     #Calculate means and quantiles
-    means = colMeans(mult.SE.list[[1]], na.rm = T)
+    means <- colMeans(mult.SE.list[[1]], na.rm = T)
     
-    means.p = colMeans(mult.SE.list[[2]], na.rm = T)
+    means.p <- colMeans(mult.SE.list[[2]], na.rm = T)
     
-    lower.ci = apply(mult.SE.list[[2]], 2, function(x) quantile(x, prob = 0.025, na.rm = T))
+    lower.ci <- apply(mult.SE.list[[2]], 2, function(x) quantile(x, prob = 0.025, na.rm = T))
     
-    upper.ci = apply(mult.SE.list[[2]], 2, function(x) quantile(x, prob = 0.975, na.rm = T))
+    upper.ci <- apply(mult.SE.list[[2]], 2, function(x) quantile(x, prob = 0.975, na.rm = T))
     
     # Return data.frame with means and quantiles
-    df = data.frame(
+    df <- data.frame(
       n.samp = 1:length(means),
       means =  means,
       lower.ci = lower.ci + (means - means.p),
@@ -81,15 +89,17 @@ multSE = function(D, nresamp = 10000, group = 1, permanova = TRUE, ... ) {
     
   } else {
     
+    if(missing(group)) group <- 1
+    
     # Conduct bootstrapping for each group
-    df = do.call(rbind, lapply(unique(group), function(igroup) {
+    df <- do.call(rbind, lapply(unique(group), function(igroup) {
       
       # Subset distance matrix by group
-      subset.D = D[group == igroup, group == igroup]
+      subset.D <- D[group == igroup, group == igroup]
       
       if(sum(subset.D) == 0) {
         data.frame(
-          group = igroup,
+          group = as.character(igroup),
           n.samp = 1,
           means =  0,
           lower.ci = NA,
@@ -98,7 +108,7 @@ multSE = function(D, nresamp = 10000, group = 1, permanova = TRUE, ... ) {
       } else {
         
         # Conduct permutation (replace = F) and boostrapped (replace = T) resampling
-        mult.SE.list = lapply(c(F, T), function(replace) {
+        mult.SE.list <- lapply(c(F, T), function(replace) {
           
           # Bootstrap subsetted distance matrix by each sample size
           do.call(cbind, lapply(2:ncol(subset.D), function(nsub) {
@@ -106,14 +116,14 @@ multSE = function(D, nresamp = 10000, group = 1, permanova = TRUE, ... ) {
             do.call(rbind, lapply(1:nresamp, function(iresamp) {
               
               # Randomly sample distance matrix
-              D.samp = subset.D[sample(1:ncol(subset.D), size = nsub, replace), sample(1:ncol(subset.D), size = nsub, replace)]
+              D.samp <- subset.D[sample(1:ncol(subset.D), size = nsub, replace), sample(1:ncol(subset.D), size = nsub, replace)]
               
               # Calculate multivariate SE based on SS
-              n = dim(as.matrix(D.samp))[1]
+              n <- dim(as.matrix(D.samp))[1]
               
-              ss = sum(D.samp^2)/n
+              ss <- sum(D.samp^2)/n
               
-              v = ss/(n-1)
+              v <- ss/(n-1)
               
               sqrt(v/n)
               
@@ -122,17 +132,17 @@ multSE = function(D, nresamp = 10000, group = 1, permanova = TRUE, ... ) {
         } )
         
         #Calculate means and quantiles
-        means = colMeans(mult.SE.list[[1]], na.rm = T)
+        means <- colMeans(mult.SE.list[[1]], na.rm = T)
         
-        means.p = colMeans(mult.SE.list[[2]], na.rm = T)
+        means.p <- colMeans(mult.SE.list[[2]], na.rm = T)
         
-        lower.ci = apply(mult.SE.list[[2]], 2, function(x) quantile(x, prob = 0.025, na.rm = T))
+        lower.ci <- apply(mult.SE.list[[2]], 2, function(x) quantile(x, prob = 0.025, na.rm = T))
         
-        upper.ci = apply(mult.SE.list[[2]], 2, function(x) quantile(x, prob = 0.975, na.rm = T))
+        upper.ci <- apply(mult.SE.list[[2]], 2, function(x) quantile(x, prob = 0.975, na.rm = T))
         
         # Return data.frame with means and quantiles
         data.frame(
-          group = igroup,
+          group = as.character(igroup),
           n.samp = 2:(length(means) + 1),
           means =  means,
           lower.ci = lower.ci + (means - means.p),
@@ -141,6 +151,7 @@ multSE = function(D, nresamp = 10000, group = 1, permanova = TRUE, ... ) {
       }
       
     } ) )
+    
   }
   
   return(df) 
--- README.md
+++ README.md
@@ -5,51 +5,60 @@
     Anderson, MJ and J Santanta-Garcon. 2015. "Measures of precision for dissimilarity-based multivariate
     analysis of ecological communities." Ecology Letters 18(1): 66-73.
     
-Version: 0.1.1 (2015-06-05) 
+Version: 0.1.2 (2017-09-18) 
 
-Author: Jon Lefcheck (jslefche@vims.edu)
+Author: Jon Lefcheck (jlefcheck@bigelow.org)
 
-##Examples
+## Examples
 
-###Load functions
+### Load functions
+
 ```
 library(devtools)
+
 # Functions from supplements
-MSEgroup.d = source_url("https://raw.githubusercontent.com/jslefche/multSE/master/R/MSEgroup_d.R")[[1]]
-MSE.d = source_url("https://raw.githubusercontent.com/jslefche/multSE/master/R/MSE_d.R")[[1]]
+MSEgroup.d <- source_url("https://raw.githubusercontent.com/jslefche/multSE/master/R/MSEgroup_d.R")[[1]]
+
+MSE.d <- source_url("https://raw.githubusercontent.com/jslefche/multSE/master/R/MSE_d.R")[[1]]
+
 # New function using vectorization
-multSE = source_url("https://raw.githubusercontent.com/jslefche/multSE/master/R/multSE.R")[[1]]
+multSE <- source_url("https://raw.githubusercontent.com/jslefche/multSE/master/R/multSE.R")[[1]]
 ```
-###Load data
+### Load data
+
 The example data represent fish survey data from Poor Knight's Island, and were collected using visual census along a 25 x 5 m transect at a depth of 8-20 m. Three time periods were sampled: September 1998 (n = 15), March 1999 (n = 21), and September 1999 (n = 20).
 ```
 # Poor Knights fish survey data, from supplementary material
-tf = tempfile()
+tf <- tempfile() 
+
 download.file("https://raw.githubusercontent.com/jslefche/multSE/master/data/PoorKnights.csv", tf)
-pk = read.csv(tf)
+
+pk <- read.csv(tf)
 ```
-###Calculate multivariate SE and confidence intervals
+### Calculate multivariate SE and confidence intervals
+
 The function `multSE` takes the following inputs: 
   -D: a species-by-community distance matrix
   -nresamp: the number of resamples to be performed
   -group: a vector of grouping variables
-  -permanova: whether inferences should be integrated across all groups (permanova = T) or calculated separately for each group (F)
+  -permanova: whether standard errors should be computed across all groups (permanova = T) or calculated separately for each group (F)
+  
 ```
 # Load vegan library
 library(vegan)
 
 # Create species-by-site distance matrix
-D = vegdist(pk[,3:49] + 1)
+D <- vegdist(pk[,3:49] + 1)
 
 # Run optimized function to generate multivariate SE for each group
-output = multSE(D, group = factor(pk$Time)) #10000 resamples
+output <- multSE(D, group = factor(pk$Time)) #10000 resamples
 ```
 And plot the output:
 ```
 # Plot output
-library(ggplot2)
+require(ggplot2)
 
-(p = ggplot(output, aes(x = n.samp, y = means, group = group)) +
+(p <- ggplot(output, aes(x = n.samp, y = means, group = group)) +
   geom_errorbar(aes(ymax = upper.ci, ymin = lower.ci), width = 0.2) +
   geom_point(aes(shape = group, fill = group), size = 4) + 
   scale_shape_manual(values = c(21, 24:25), name = "") +
@@ -64,12 +73,13 @@ library(ggplot2)
 ![multSE plot](https://github.com/jslefche/jslefche.github.io/blob/master/img/multSE_plot.jpeg?raw=true)
 
 It appears that there is no appreciable gain in precision (i.e., decrease in SE) after about n = 15 samples, but we can quantitatively double-check this estimate using the `minsamp` function:
+
 ```
 # Load function
-minsamp = source_url("https://raw.githubusercontent.com/jslefche/multSE/master/R/minsamp.R")[[1]]
+minsamp <- source_url("https://raw.githubusercontent.com/jslefche/multSE/master/R/minsamp.R")[[1]]
 
 # Calculate minimum sample size for each group
-(minimum.sample.size = minsamp(output, output$group))
+(minimum.sample.size <- minsamp(output, output$group))
   
 #  group  min.mean min.lower.ci min.upper.ci min.n
 # Sep.98 0.1123218   0.10133429    0.1213122    11
@@ -87,7 +97,7 @@ p +
 Now repeat, but integrate across groups using residuals from a PERMANOVA (instead of SS):
 
 ```
-output2 = multSE(D, group = factor(pk$Time), permanova = T) #10000 resamples
+output2 <- multSE(D, group = factor(pk$Time), permanova = T) #10000 resamples
 
 ggplot(output2, aes(x = n.samp, y = means)) +
   geom_errorbar(aes(ymax = upper.ci, ymin = lower.ci), width = 0.2)+
@@ -108,7 +118,7 @@ Compare `multSE` to function included in the supplements of ELE paper `MSEgroup.
 
 ```
 # Calculate system time for old vs. new function for a variety of resample sizes 
-benchmarks.df = do.call(rbind, lapply(c(10, 100, 1000, 10000), function(i)
+benchmarks.df <- do.call(rbind, lapply(c(10, 100, 1000, 10000), function(i)
   data.frame(
     nresamp = i,
     fn = c("old", "new"),
@@ -131,8 +141,9 @@ ggplot(benchmarks.df, aes(x = nresamp, y = time, group = fn, col = fn, shape = f
 As you can see, the new function `multSE` is much faster, particularly when the number of resamples is large.
 
 Now repeat for `permanova = T`:
+
 ```
-benchmarks.df2 = do.call(rbind, lapply(c(10, 100, 1000, 10000), function(i)
+benchmarks.df2 <- do.call(rbind, lapply(c(10, 100, 1000, 10000), function(i)
   data.frame(
     nresamp = i,
     fn = c("old", "new"),
