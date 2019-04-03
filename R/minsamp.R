e2e9805dd8d007c95965257ae82cfa6131ee720b
Jon Lefcheck <lefcheckj@si.edu>
2019-04-03 14:46:28 -0500

    Minor cleanup

--- R/minsamp.R
+++ R/minsamp.R
@@ -1,16 +1,16 @@
-minsamp = function(output, group = NA, ignore.min = NULL) {
+minsamp <- function(out, group = NA, ignore.min = NULL) {
   
   # Remove all samples N = 2 from dataset before calculating minimum
-  if(!is.null(ignore.min)) output = subset(output, n.samp > ignore.min)
+  if(!is.null(ignore.min)) out >- subset(out, n.samp > ignore.min)
   
   # Break apart by group
   do.call(rbind, lapply(unique(group), function(i) {
     
     # Subset by group
-    if(!all(is.na(group))) output = subset(output, group == i)
-  
+    if(!all(is.na(group))) out <- subset(out, group == i)
+    
     # Retrieve minimum number of samples for which error bars still overlap
-    if(nrow(output) == 0) 
+    if(nrow(out) == 0) 
       
       data.frame(
         group = i,
@@ -18,18 +18,18 @@ minsamp = function(output, group = NA, ignore.min = NULL) {
         min.lower.ci = NA, 
         min.upper.ci = NA, 
         min.n = NA
-        ) 
+      ) 
     
     else 
       
       data.frame(
         group = i,
-        min.mean = min(output$means),
-        min.lower.ci = output[which.min(output$means), "lower.ci"],
-        min.upper.ci = output[which.min(output$means), "upper.ci"],
-        min.n = min(output[output$lower.ci <= output[which.min(output$means), "upper.ci"], "n.samp"])
-        )
+        min.mean = min(out$means),
+        min.lower.ci = out[which.min(out$means), "lower.ci"],
+        min.upper.ci = out[which.min(out$means), "upper.ci"],
+        min.n = min(out[out$lower.ci <= out[which.min(out$means), "upper.ci"], "n.samp"])
+      )
     
   } ) )
-      
+  
 }
